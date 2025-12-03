b8f431668ad27b64a054861150007678f12a7260
RumeshChathuranga <rumarumesh1220@gmail.com>
2025-12-03 15:04:41 +0530

    print list

--- LinkedList/src/main.java
+++ LinkedList/src/main.java
@@ -1,5 +1,11 @@
 public class main {
     public static void main(String[] args) {
         LinkedList mylinkedlist = new LinkedList(4);
+
+        mylinkedlist.getHead();
+        mylinkedlist.getTail();
+        mylinkedlist.getLength();
+
+        mylinkedlist.printList();
     }
 }
