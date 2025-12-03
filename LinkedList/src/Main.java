ba62f941b18c73eef0b787a495f92278aa42425f
RumeshChathuranga <rumarumesh1220@gmail.com>
2025-12-03 16:28:09 +0530

    Implement LL set method

--- LinkedList/src/Main.java
+++ LinkedList/src/Main.java
@@ -1,9 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         LinkedList mylinkedlist = new LinkedList(2);
-        mylinkedlist.prepend(1);
+        mylinkedlist.append(3);
+        mylinkedlist.append(4);
+        mylinkedlist.append(5);
+        mylinkedlist.append(6);
         mylinkedlist.printList();
-        System.out.println(mylinkedlist.get(1).value);
-
+        mylinkedlist.set(2,100);
+        mylinkedlist.printList();
     }
 }
