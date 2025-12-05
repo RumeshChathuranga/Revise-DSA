3fdbfdc44d8e825e966c5ead30106913f6dceba5
RumeshChathuranga <rumarumesh1220@gmail.com>
2025-12-05 19:32:41 +0530

    Create Stack constructor

---
+++ Stack/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Stack stk = new Stack(7);
+        stk.getTop();
+        stk.getHeight();
+        stk.printStack();
+    }
+}
---
+++ Stack/src/Stack.java
@@ -0,0 +1,34 @@
+public class Stack {
+    private Node top;
+    private int height;
+
+    class Node{
+        int value;
+        Node next;
+
+        public Node(int value){
+            this.value = value;
+        }
+    }
+
+    public Stack(int value){
+        Node newnode = new Node(value);
+        top = newnode;
+        height = 1;
+    }
+
+    public void printStack(){
+        Node temp = top;
+        while(temp != null){
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+    public void getTop(){
+        System.out.println("Top : " + top.value);
+    }
+
+    public void getHeight(){
+        System.out.println("Height : " + height);
+    }
+}
