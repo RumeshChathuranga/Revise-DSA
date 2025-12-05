30de932aba7bf4156e4a389188bc1863e098c315
RumeshChathuranga <rumarumesh1220@gmail.com>
2025-12-05 20:20:00 +0530

    Implement Queue constructor.

---
+++ Queue/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Queue q = new Queue(7);
+        q.getFirst();
+        q.getLast();
+        q.getLength();
+        q.printQueue();
+    }
+}
---
+++ Queue/src/Queue.java
@@ -0,0 +1,44 @@
+public class Queue {
+    private Node first;
+    private Node last;
+    private int length;
+
+    class Node {
+        int value;
+        Node next;
+
+        public Node(int value){
+            this.value = value;
+        }
+    }
+
+    public Queue(int value){
+        Node newnode = new Node(value);
+        first = newnode;
+        last = newnode;
+        length = 1;
+    }
+
+    public void printQueue() {
+        Node temp = first;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void getFirst() {
+        System.out.println("First: " + first.value);
+
+    }
+
+    public void getLast() {
+        System.out.println("Last: " + last.value);
+
+    }
+
+    public void getLength() {
+        System.out.println("Length: " + length);
+
+    }
+}
