d142861f25f4b6c3b831b424bee8215b39a55ca7
RumeshChathuranga <rumarumesh1220@gmail.com>
2025-12-07 00:55:47 +0530

    Create BST instance

---
+++ BST/src/BinarySearchTree.java
@@ -0,0 +1,14 @@
+public class BinarySearchTree {
+    Node root;
+
+    class Node{
+        int value;
+        Node left;
+        Node right;
+        public Node(int value){
+            this.value = value;
+        }
+    }
+
+
+}
---
+++ BST/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        BinarySearchTree bst = new BinarySearchTree();
+        System.out.println("root = " + bst.root);
+    }
+}
