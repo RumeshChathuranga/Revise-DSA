72ea3664a7ff9bbb2fe50ec0544760ceca8c2c4e
RumeshChathuranga <rumarumesh1220@gmail.com>
2025-12-07 01:28:26 +0530

    implement insert method

--- BST/src/BinarySearchTree.java
+++ BST/src/BinarySearchTree.java
@@ -10,5 +10,31 @@ public class BinarySearchTree {
         }
     }
 
+    public boolean insert(int value){
+        Node newnode = new Node(value);
+        if (root == null) {
+            root = newnode;
+            return true;
+        }
+        Node temp = root;
+
+        while(true){
+           if(newnode.value == temp.value) return false;
+           if(newnode.value<temp.value){
+               if(temp.left == null){
+                   temp.left = newnode;
+                   return true;
+               }
+               temp = temp.left;
+           }else{
+               if(temp.right == null){
+                   temp.right=newnode;
+                   return true;
+               }
+               temp = temp.right;
+           }
+
+        }
+    }
 
 }
--- BST/src/Main.java
+++ BST/src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         BinarySearchTree bst = new BinarySearchTree();
-        System.out.println("root = " + bst.root);
+        bst.insert(7);
+        bst.insert(21);
+        bst.insert(76);
+        bst.insert(17);
+        bst.insert(57);
+        bst.insert(87);
     }
+
+
 }
