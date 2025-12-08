33b649cdff5be273766786dcf7b0f52b75af9594
RumeshChathuranga <rumarumesh1220@gmail.com>
2025-12-08 16:27:04 +0530

    Leetcode 160 problem sloved

---
+++ Leetcode/Q160-answer.java
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode a = headA;
+        ListNode b = headB;
+
+        while(a!=b){
+            a = (a == null)? a = headB: a.next;
+            b = (b == null)? b = headA: b.next;
+        }
+        return a;
+    }
+}
