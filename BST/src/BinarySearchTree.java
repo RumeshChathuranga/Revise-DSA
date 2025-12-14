0da988ae75cd663a44b9cb653c8d09420318fabf
RumeshChathuranga <rumarumesh1220@gmail.com>
2025-12-14 17:57:51 +0530

    Implement BFS for bst

--- BST/src/BinarySearchTree.java
+++ BST/src/BinarySearchTree.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BinarySearchTree {
     Node root;
 
@@ -108,6 +112,24 @@ public class BinarySearchTree {
     }
     public void deleteNode(int value){
         deleteNode(root, value);
+    }
+
+    public ArrayList<Integer> BFS(){
+        Node currentNode = root;
+        Queue<Node> queue = new LinkedList<>();
+        ArrayList<Integer> results = new ArrayList<>();
+        queue.add(currentNode);
+        while(!queue.isEmpty()){
+            currentNode = queue.remove();
+            results.add(currentNode.value);
+            if(currentNode.left != null){
+                queue.add(currentNode.left);
+            }
+            if(currentNode.right != null){
+                queue.add(currentNode.right);
+            }
+        }
+        return  results;
     }
 
 }
--- BST/src/Main.java
+++ BST/src/Main.java
@@ -7,9 +7,7 @@ public class Main {
         bst.rinsert(17);
         bst.rinsert(57);
         bst.rinsert(87);
-        System.out.println(bst.contains(76));
-        bst.deleteNode(76);
-        System.out.println(bst.contains(76));
+        System.out.println(bst.BFS());
     }
 
 
