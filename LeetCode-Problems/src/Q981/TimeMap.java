4658e6ab50e72ace1a4b23674bd9ff7fe26a16c4
RumeshChathuranga <Rumarumesh1220@gmail.com>
2026-01-21 17:26:32 +0530

    Leetcode: problem 981

---
+++ LeetCode-Problems/src/Q981/TimeMap.java
@@ -0,0 +1,32 @@
+package Q981;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+class TimeMap {
+    private Map<String, TreeMap<Integer,String >> map;
+    public TimeMap() {
+        map = new HashMap<>();
+    }
+
+    public void set(String key, String value, int timestamp) {
+        map.computeIfAbsent(key, v -> (new TreeMap<>())).put(timestamp, value);
+    }
+
+    public String get(String key, int timestamp) {
+        TreeMap<Integer,String> tmap = map.get(key);
+        if (tmap == null) {
+            return "";
+        }
+        Map.Entry<Integer, String> entry = tmap.floorEntry(timestamp);
+        return entry == null ? "" : entry.getValue();
+    }
+}
+
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * TimeMap obj = new TimeMap();
+ * obj.set(key,value,timestamp);
+ * String param_2 = obj.get(key,timestamp);
+ */
