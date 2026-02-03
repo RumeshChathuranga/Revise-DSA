851f083af6ce456f6bb4dcc0d8360b165698ae5f
RumeshChathuranga <Rumarumesh1220@gmail.com>
2026-02-03 23:39:49 +0530

    Leetcode: problem 287

---
+++ LeetCode-Problems/src/Q287/Solution.java
@@ -0,0 +1,18 @@
+package Q287;
+
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int slow = nums[0];
+        int fast = nums[nums[0]];
+        while(slow!=fast) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }
+        slow = nums[0];
+        while(slow!=fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
+}
