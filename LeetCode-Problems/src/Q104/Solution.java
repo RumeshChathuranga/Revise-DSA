1f8be219ded1ddb923cccb11d32f02338f442999
RumeshChathuranga <Rumarumesh1220@gmail.com>
2026-04-06 23:11:21 +0530

    Leetcode: problem 104

---
+++ LeetCode-Problems/src/Q104/Solution.java
@@ -0,0 +1,24 @@
+package Q104;
+
+public class Solution {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+
+        int leftmax = maxDepth(root.left);
+        int rightmax = maxDepth(root.right);
+        return Math.max(leftmax, rightmax) + 1;
+    }
+
+}
