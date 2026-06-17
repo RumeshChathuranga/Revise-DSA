4b9bf6f512209593fc6592ef33a3334694b26a24
RumeshChathuranga <Rumarumesh1220@gmail.com>
2026-06-17 15:25:36 +0530

    Leetcode: problem 110

---
+++ LeetCode-Problems/src/Q110/Solution.java
@@ -0,0 +1,32 @@
+package Q110;
+
+class Solution {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(){}
+        TreeNode(int val){this.val = val;}
+        TreeNode(int val, TreeNode left, TreeNode right){
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public boolean isBalanced(TreeNode root) {
+        if(root == null) return true;
+        int leftHeight = getHeight(root.left);
+        int rightHeight = getHeight(root.right);
+
+        if(Math.abs(leftHeight - rightHeight) > 1) return false;
+        return isBalanced(root.left) && isBalanced(root.right);
+    }
+
+    private int getHeight(TreeNode node){
+        if(node == null) return 0;
+        int left = getHeight(node.left);
+        int right = getHeight(node.right);
+
+        return Math.max(left, right)+1;
+    }
+}
