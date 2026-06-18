2494d969856bb45fd618dd12ab862ef320b75151
RumeshChathuranga <Rumarumesh1220@gmail.com>
2026-06-18 15:28:56 +0530

    Leetcode: problem 235

---
+++ LeetCode-Problems/src/Q235/Solution.java
@@ -0,0 +1,35 @@
+package Q235;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+class Solution {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        int parentval = root.val;
+        int pval = p.val;
+        int qval = q.val;
+
+        if (pval > parentval &&  qval > parentval) {
+            return lowestCommonAncestor(root.right, p, q);
+        }
+        if (pval < parentval && qval < parentval) {
+            return lowestCommonAncestor(root.left, p, q);
+        }
+        return root;
+    }
+}
