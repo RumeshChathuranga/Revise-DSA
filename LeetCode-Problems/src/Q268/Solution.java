13747610a88bf64a2fe49b92f982907f00686e1b
RumeshChathuranga <Rumarumesh1220@gmail.com>
2026-06-21 14:56:07 +0530

    Leetcode: problem 268

---
+++ LeetCode-Problems/src/Q268/Solution.java
@@ -0,0 +1,16 @@
+package Q268;
+
+class Solution {
+    public int missingNumber(int[] nums) {
+        int xor = 0;
+
+        for (int i = 0; i<= nums.length; i++){
+            xor ^= i;
+        }
+
+        for (int j = 0; j< nums.length; j++){
+            xor ^= nums[j];
+        }
+        return xor;
+    }
+}
