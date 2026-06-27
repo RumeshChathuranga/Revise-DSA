85c501fc2f8ce9e9f68949bed4a62ae96e70242e
RumeshChathuranga <Rumarumesh1220@gmail.com>
2026-06-27 21:52:08 +0530

    Leetcode: problem 43

---
+++ LeetCode-Problems/src/Q43/Solution.java
@@ -0,0 +1,33 @@
+package Q43;
+
+class Solution {
+    public String multiply(String num1, String num2) {
+        if(num1.equals("0") || num2.equals("0")) return "0";
+
+        int[] results = new int[num1.length()+num2.length()];
+
+        for(int i = num1.length() - 1; i>= 0; i--){
+            for(int j = num2.length() - 1; j >= 0; j--){
+                int digit1 = num1.charAt(i) - '0';
+                int digit2 = num2.charAt(j) - '0';
+                int mul = digit1*digit2;
+
+                int posLow = i + j + 1;
+                int posHigh = i + j;
+
+                int sum = mul + results[posLow];
+
+                results[posLow] = sum%10;
+                results[posHigh] += sum/10;
+            }
+        }
+
+        StringBuilder product = new StringBuilder();
+        for(int num : results){
+            if(!(product.isEmpty() && num == 0)){
+                product.append(num);
+            }
+        }
+        return product.isEmpty() ? "0" : product.toString();
+    }
+}
