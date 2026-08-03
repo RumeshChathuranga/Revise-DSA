ef0db7b1fd22dde6d4c94fa5a9996dc0a098a965
RumeshChathuranga <Rumarumesh1220@gmail.com>
2026-08-04 02:36:29 +0530

    Leetcode: problem 78

---
+++ LeetCode-Problems/src/Q78/Solution.java
@@ -0,0 +1,21 @@
+package Q78;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        generateSubsets(0, nums, new ArrayList<>(), result);
+        return result;
+     }
+
+    private void generateSubsets(int index, int[] nums, List<Integer> current, List<List<Integer>> result) {
+        result.add(new ArrayList<>(current));
+        for (int i = index; i < nums.length; i++) {
+            current.add(nums[i]);
+            generateSubsets(i + 1, nums, current, result);
+            current.removeLast();
+        }
+    }
+}
